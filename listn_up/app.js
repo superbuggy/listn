59b531cd2585f9134af54cab4b127311f2c8ad3e
James Reichard <jpqreichard@gmail.com>
2016-11-19 16:12:37 -0500

    add ui router, state, and associated view

--- listn_up/app.js
+++ listn_up/app.js
@@ -1,22 +1,36 @@
 angular
   .module("listn", [
+    "ui.router",
     "ngResource"
   ])
-  .controller("ListnController", [
-    "ListnFactory", 
-    ListnControllerFunc
+  .config([
+    "$stateProvider",
+    RouterFunc
   ])
-  .factory("ListnFactory", [
-    "$resource", 
-    ListnFactoryFunc
+  .controller("ListnsController", [
+    "ListnsFactory",
+    ListnsControllerFunc
+  ])
+  .factory("ListnsFactory", [
+    "$resource",
+    ListnsFactoryFunc
   ])
 
-function ListnControllerFunc(ListnFactory) {
-  this.listns = ListnFactory.query();  
+function RouterFunc($stateProvider) {
+  $stateProvider
+    .state("listnsIndex", {
+      url: "/",
+      templateUrl: "js-views/listns-index.html",
+      controller: "ListnsController",
+      controllerAs: "listnsVm"
+    })
 }
 
+function ListnsControllerFunc(ListnsFactory) {
+  this.listns = ListnsFactory.query();
+}
 
-function ListnFactoryFunc($resource){
+function ListnsFactoryFunc($resource){
   return $resource("http://localhost:3000/listns/:id.json", {}, {
       update: { method: "PUT" }
     });
