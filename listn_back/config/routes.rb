411a9576f4f941f2ba2f40d91c512289858de87e
James Reichard <jpqreichard@gmail.com>
2016-11-20 17:33:13 -0500

    comment out redundant code

--- listn_back/config/routes.rb
+++ listn_back/config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  get 'listns_controller/index'
-
-  get 'listns_controller/show'
-
-  get 'listns_controller/new'
-
-  get 'listns_controller/create'
-
-  get 'listns_controller/edit'
-
-  get 'listns_controller/update'
-
-  get 'listns_controller/destroy'
+  # get 'listns_controller/index'
+  #
+  # get 'listns_controller/show'
+  #
+  # get 'listns_controller/new'
+  #
+  # get 'listns_controller/create'
+  #
+  # get 'listns_controller/edit'
+  #
+  # get 'listns_controller/update'
+  #
+  # get 'listns_controller/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :listns do
