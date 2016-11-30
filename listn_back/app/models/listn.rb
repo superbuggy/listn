8c9728c9c06556627f3c7f2e48ede08518196606
James Reichard <jpqreichard@gmail.com>
2016-11-29 22:40:38 -0500

    deploy to gh-pages

--- listn_back/app/models/listn.rb
+++
@@ -1,3 +0,0 @@
-class Listn < ApplicationRecord
-  has_many :notes, dependent: :destroy
-end
--- listn_back/db/migrate/20161119215918_add_listn_ref_to_notes.rb
+++
@@ -1,5 +0,0 @@
-class AddListnRefToNotes < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :notes, :listn, foreign_key: true
-  end
-end
